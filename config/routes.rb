76343e0f923d8682237461277bf3569ef02fbe7a
hayashi-7 <tomos19088@gmail.com>
2020-10-01 16:20:09 +0900

    マイページ 機能

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,11 @@ Rails.application.routes.draw do
     member do
     get 'category'
     end
+    collection do
+      get 'search'
+    end
   end
   root to: 'books#index'
   get 'tags/:tag', to: 'books#index', as: :tag
+  resources :users, only: :show
 end
