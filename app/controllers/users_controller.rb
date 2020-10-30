ecdbde159cc5ec1a929cca32f159a18b2ccfe95f
hayashi-7 <tomos19088@gmail.com>
2020-10-30 13:56:44 +0900

    view調整

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -17,7 +17,7 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     @books = @user.books
-    @likes = Like.where(user_id: @user.id)
+    @likes = Like.where(user_id: @user.id).order('created_at DESC').limit(5)
   end
 
   def following
