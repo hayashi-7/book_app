0a3011219126d9f380d1dd0873158ef0d59fcf71
hayashi-7 <tomos19088@gmail.com>
2020-12-06 01:25:27 +0900

    Update deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -10,7 +10,7 @@ set :rbenv_ruby, File.read('.ruby-version').strip
 set :deploy_to, '/var/www/book_app'
 # どの公開鍵を利用してデプロイするか
 set :ssh_options, auth_methods: ['publickey'],
-                  keys: ['~/Desktop/aws-book-key-ssh.pem'] 
+                  keys: ['~/Desktop/book_app_ssh.pem'] 
 # Nginxの設定ファイル名と置き場所を修正
 set :nginx_config_name, "#{fetch(:application)}.conf"
 set :nginx_sites_enabled_path, "/etc/nginx/conf.d"
@@ -19,20 +19,14 @@ append :linked_files, "config/master.key"
 append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "node_modules"
 
 # プロセス番号を記載したファイルの場所
-set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
 
-# Unicornの設定ファイルの場所
-set :unicorn_config_path, -> { "#{current_path}/config/unicorn.rb" }
 set :keep_releases, 5
 
 set :linked_files, %w{ config/master.key }
 
 after 'deploy:publishing', 'deploy:restart'
 namespace :deploy do
-  task :restart do
-    invoke 'unicorn:stop'
-    invoke 'unicorn:start'
-  end
+  
 
   desc 'upload master.key'
   task :upload do
